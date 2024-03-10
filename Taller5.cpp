14266bed4ce9b450957d11b69be1ceee4bcb5980
HauuF <Padre1000@outlook.es>
2024-03-10 17:09:13 -0500

    Completo

--- Taller5.cpp
+++ Taller5.cpp
@@ -5,14 +5,39 @@
 
 using namespace std;
 
+bool comparar(int* puntero1, int* puntero2, List<int>& lista){
+    int Address1, Address2;
+    for(int i = 0; i < lista.size; i++){
+        if(lista.getAddressOf(i) == puntero1){
+            Address1 = i;
+        }
+        if(lista.getAddressOf(i) == puntero2){
+            Address2 = i;
+        }
+    }
+    return lista.get(Address1) < lista.get(Address2);
+}
+
+void ordenarListaPunteros(List<int*>& listpointer, List<int>& lista) {
+    for (int i = 0; i < listpointer.size - 1; i++) {
+        for (int j = 0; j < listpointer.size - i - 1; j++) {
+            int* Address1 = listpointer.get(j);
+            int* Address2 = listpointer.get(j + 1);
+            if (!comparar(Address1, Address2, lista)) {
+                listpointer.set(j, Address2);
+                listpointer.set(j + 1, Address1);
+            }
+        }
+    }
+}
+
 void printMenu();
 
 int main() {
     
-    
     int option = 0;
     List<int> lista;
-
+    List<int*> listpointer;
 
     do {
         printMenu();
@@ -48,10 +73,11 @@ int main() {
             {
                 cout << endl;
                 cout << "Opción 2" << endl;
-
-
-
-
+                for(int i = 0; i < lista.size; i++){
+                    int* listapuntador = lista.getAddressOf(i);
+                    listpointer.add(listapuntador);
+                }
+                cout << "Lista de apuntadores creada." << endl;
                 cout << endl;
             }
                 break;
@@ -59,10 +85,8 @@ int main() {
             {
                 cout << endl;
                 cout << "Opción 3" << endl;
-
-
-
-
+                ordenarListaPunteros(listpointer, lista);
+                cout << "Lista de apuntadores ordenada." << endl;
                 cout << endl;
             }
                 break;
@@ -79,10 +103,9 @@ int main() {
             case 5:
             {
                 cout << "Opción 5" << endl;
-
-
-
-
+                for(int i = 0; i <  listpointer.size; i++){
+                    cout << "Miembro #"<< i + 1 <<": "<< listpointer.get(i) <<" "<< endl;
+                }
                 cout << endl;
             }
                 break;
